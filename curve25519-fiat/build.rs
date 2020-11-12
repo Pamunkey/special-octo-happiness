292f48b0862716da6a7eb6f9463e2f6b3a54b9dc
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2020-11-12 20:47:15 +0000

    Update djangojs.po (POEditor.com)

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -1,3 +1,4 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 //
-// Licensed under the Apache License, Version 2.0 (the "License")
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you
