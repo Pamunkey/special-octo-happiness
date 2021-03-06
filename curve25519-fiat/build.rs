aebc5dcface8cbe7ef893424bd7f1ab63443d073
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2021-03-06 19:59:52 +0000

    Update CI.yml

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -8,4 +8,4 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY K
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, eit
