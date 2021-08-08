91244670bd6ac034f74d98bbe2a9e9e5dff743ee
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2021-08-08 21:57:50 +0000

    updated package info

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -19,4 +19,4 @@ macro_rules! get(($name:expr) => (ok!(env::var($name))));
 macro_rules! ok(($expression:expr) => ($expression.unwrap()));
 
 pub const FIAT_REPO: &str = "https://github.com/mit-plv/fiat-crypto.git";
-pub const FIAT_HASH: &str =
+pub const FIAT_HASH: &str = "c96
