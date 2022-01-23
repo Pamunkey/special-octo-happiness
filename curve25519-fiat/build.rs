f0fc1257bae3e58a46deda8d7e1c225ffc36b6ea
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2022-01-23 20:07:50 +0000

    Updating Gemfile.lock

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -43,4 +43,4 @@ fn main() {
     // this folder
     let basedir = PathBuf::from(&get!("CARGO_MANIFEST_DIR"));
     let src_dir = basedir.join("src");
-    println!("Dir 
+    println!("Dir is: {:?}
