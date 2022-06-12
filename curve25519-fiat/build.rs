28ce4bb1b2e514d943901cc9f6d58f706fb82e76
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2022-06-12 19:38:53 +0000

    Global update

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -57,4 +57,5 @@ fn main() {
     let cond = if let Ok(mut file) = fs::File::open(src_path) {
         let mut sha256 = Sha256::new();
         io::copy(&mut file, &mut sha256).expect("failed to copy file");
-        let hash = sha256.result(
+        let hash = sha256.result();
+   
