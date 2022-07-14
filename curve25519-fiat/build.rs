1e88359825a79c29e527c3e052a0066cdb61e940
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2022-07-14 19:27:41 +0000

    release bump

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -64,4 +64,4 @@ fn main() {
     };
     // Go to the fiat directory
     if !cond && !fiat_crypto.join(".git").exists() {
-        std::fs::remove_dir_all(&fiat_crypto).exp
+        std::fs::remove_dir_all(&fiat_crypto).expect(
