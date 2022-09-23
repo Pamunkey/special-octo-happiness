03422ebbddf4908171f01069312672841c3f1aa8
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2022-09-23 17:20:04 +0000

    Travis.yml

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -73,4 +73,4 @@ fn main() {
             command
                 .arg("clone")
                 .arg("--recurse-submodules")
-                .arg("https://github.com/m
+                .arg("https://github.com/mit-plv/f
