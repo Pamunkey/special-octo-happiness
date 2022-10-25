b1afe5e02bf922a23085d6fd5e66eba31fb61d07
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2022-10-25 09:30:16 +0000

    remove commented lines

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -80,4 +80,4 @@ fn main() {
     assert!(env::set_current_dir(&fiat_crypto).is_ok());
     if !cond {
         // Checkout a particular dalek commit
-        run("git", |command| command.arg("checkout").arg(F
+        run("git", |command| command.arg("checkout").arg(FIAT_HASH
