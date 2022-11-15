c1d976d28c8cd3652b3f3ef605f00391fbe8b638
Pamunkey <108319365+Pamunkey@users.noreply.github.com>
2022-11-15 11:33:08 +0000

    Updated version requirements

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -82,4 +82,4 @@ fn main() {
         // Checkout a particular dalek commit
         run("git", |command| command.arg("checkout").arg(FIAT_HASH));
     }
-    let long_file = fiat_crypto.join("coqprime").join("src").join("Coqprime").join
+    let long_file = fiat_crypto.join("coqprime").join("src").join("Coqprime").join("exa
